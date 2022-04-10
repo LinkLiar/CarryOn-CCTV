676c547982a6947846c74dc90fa1cc217a442114
Link <38213500+LinkLiar@users.noreply.github.com>
2022-04-10 23:51:01 +0800

    update

--- CCTV_R.py
+++ CCTV_R.py
@@ -1,127 +1,23 @@
-# coding:utf-8
-import paho.mqtt.client as mqtt
-import time
-import sys
-import base64
-import webbrowser
-from time import sleep
-
-HOST = 'xxx.xxx.xxx.xxx'
-PORT = 1883
-client_id = 'CCTV-R'
-client = mqtt.Client(client_id)
-
-# 命名生成的html
-GEN_HTML = "index.html"
-message = """
-<!DOCTYPE > 
-<html xmlns="http://www.w3.org/1999/xhtml"> 
-<head> 
-<meta charset="utf-8" /> 
-<title>监控中......</title> 
-        <script language="JavaScript">
-            function startTime()   
-            {   
-                var today=new Date();//定义日期对象   
-                var yyyy = today.getFullYear();//通过日期对象的getFullYear()方法返回年    
-                var MM = today.getMonth()+1;//通过日期对象的getMonth()方法返回年    
-                var dd = today.getDate();//通过日期对象的getDate()方法返回年     
-                var hh=today.getHours();//通过日期对象的getHours方法返回小时   
-                var mm=today.getMinutes();//通过日期对象的getMinutes方法返回分钟   
-                var ss=today.getSeconds();//通过日期对象的getSeconds方法返回秒   
-                // 如果分钟或小时的值小于10，则在其值前加0，比如如果时间是下午3点20分9秒的话，则显示15：20：09   
-                MM=checkTime(MM);
-                dd=checkTime(dd);
-                hh=checkTime(hh);
-                mm=checkTime(mm);   
-                ss=checkTime(ss);    
-                var day; //用于保存星期（getDay()方法得到星期编号）
-                if(today.getDay()==0)   day   =   "星期日 " 
-                if(today.getDay()==1)   day   =   "星期一 " 
-                if(today.getDay()==2)   day   =   "星期二 " 
-                if(today.getDay()==3)   day   =   "星期三 " 
-                if(today.getDay()==4)   day   =   "星期四 " 
-                if(today.getDay()==5)   day   =   "星期五 " 
-                if(today.getDay()==6)   day   =   "星期六 " 
-                document.getElementById('nowDateTimeSpan').innerHTML=yyyy+"-"+MM +"-"+ dd +" " + hh+":"+mm+":"+ss+"   " + day;   
-                setTimeout('startTime()',1000);//每一秒中重新加载startTime()方法 
-            }   
-             
-            function checkTime(i)   
-            {   
-                if (i<10){
-                    i="0" + i;
-                }   
-                  return i;
-            }  
-        </script>
-<style> 
-.divcss5{text-align:center} 
-</style> 
-</head> 
-<body> 
-<div class="divcss5"><img src="Cam1.jpg" /></div> 
-<body onload="startTime()">
-	   <center>该帧时间：<font color="#000000">%s</font> </center>
-        <center>当前时间：<font color="#000000"><span id="nowDateTimeSpan"></span></font> </center>
-</body>
-</body> 
-</html> 
-"""
+import os
+import importlib.machinery
+import importlib.util
 
 
-def Base64ToImage(imageRec):
-    imgData = base64.b64decode(imageRec)
-    imageHandle = open('Cam1.jpg', 'wb')
-    RealTime = time.localtime(time.time() + 28800)    # 与北京时间的偏移
-    print(time.strftime("%Y-%m-%d %H:%M:%S", RealTime))
-    imageHandle.write(imgData)
-    imageHandle.close()
-
-    # 打开文件，准备写入
-    f = open(GEN_HTML, 'w')
-    DayNumber = time.strftime("%w", RealTime)
-    if(DayNumber == '0'):
-        day = "星期日"
-    if(DayNumber == '1'):
-        day = "星期一"
-    if(DayNumber == '2'):
-        day = "星期二"
-    if(DayNumber == '3'):
-        day = "星期三"
-    if(DayNumber == '4'):
-        day = "星期四"
-    if(DayNumber == '5'):
-        day = "星期五"
-    if(DayNumber == '6'):
-        day = "星期六"
-    text = time.strftime("%Y-%m-%d %H:%M:%S", RealTime) + ' ' + day
-
-
-    f.write(message % text)    # 写入文件
-    f.close()    # 关闭文件
-    webbrowser.open(GEN_HTML, new=1)
-
-
-def OnMessageComeCmd(client, userdata, msg):
-    Base64ToImage(msg.payload.decode("UTF-8"))
-    print("RECEIVE Successfully")
-
-
-def OnConnect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
-
-def client_connect():
-    client.username_pw_set("CCTV_R", "8qADTw1m6ilcHg0pVSmh")
-    client.on_connect = OnConnect
-    client.connect(HOST, PORT, 60)
-    client.loop_start()
-    client.subscribe("Cam1", 1)
-    client.on_message = OnMessageComeCmd
-
-
-if __name__ == '__main__':
-    client_connect()
-    while(True):
-        time.sleep(20)
+def path_import(file):
+    loader_details = (
+        importlib.machinery.ExtensionFileLoader,
+        importlib.machinery.EXTENSION_SUFFIXES
+    )
+    tools_finder = importlib.machinery.FileFinder(
+        os.path.dirname(file), loader_details)
+    print("FileFinder: ", tools_finder)
+    toolbox_specs = tools_finder.find_spec(
+        os.path.basename(file).split(".")[0])
+    print("find_spec: ", toolbox_specs)
+    toolbox = importlib.util.module_from_spec(toolbox_specs)
+    print("module: ", toolbox)
+    toolbox_specs.loader.exec_module(toolbox)
+    print("导入成功 path_import(): ", toolbox)
+    print("检查sys中是否包含了此模块: ", toolbox in sys.modules)
+    print("******************* 动态加载模块完成 *************************\n")
+    return toolbox
--- CCTV_S.py
+++ CCTV_S.py
@@ -8,8 +8,9 @@ import cv2
 HOST = 'xxx.xxx.xxx.xxx'    # 设置成你服务器的IP
 PORT = 1883
 clientID = 'CCTV-S'
-width = 800
-height = 600
+width = 1920
+height = 1080
+
 
 def ImageToBase64(image_np):
     image = cv2.imencode('.jpg', image_np)[1]
@@ -18,47 +19,47 @@ def ImageToBase64(image_np):
 
 
 def SendingImage():
-    cap = cv2.VideoCapture(0)    # 设置成摄像头的ID
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH,width);
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT,height);
+    cap = cv2.VideoCapture(1)    # 设置成摄像头的ID
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
     print("Camera initialized successfully...")
     while True:
         ret, frame = cap.read()
         disconnectTime = 0
-        while(not ret):
-            tic = time.time()
-            time.sleep(6)
-            cap = cv2.VideoCapture(0)
-            cap.set(cv2.CAP_PROP_FRAME_WIDTH,width);
-            cap.set(cv2.CAP_PROP_FRAME_HEIGHT,height);
-            ret, frame = cap.read()
-            toc = time.time()
-            disconnectTime += toc-tic
-            print("Camera disconnected...for " +
-                  f"{disconnectTime:.4f}" + " seconds")
+        # while(not ret):
+        #     tic = time.time()
+        #     time.sleep(6)
+        #     cap = cv2.VideoCapture(0)
+        #     cap.set(cv2.CAP_PROP_FRAME_WIDTH,width);
+        #     cap.set(cv2.CAP_PROP_FRAME_HEIGHT,height);
+        #     ret, frame = cap.read()
+        #     toc = time.time()
+        #     disconnectTime += toc-tic
+        #     print("Camera disconnected...for " +
+        #           f"{disconnectTime:.4f}" + " seconds")
 
-        data = ImageToBase64(frame)
+        # data = ImageToBase64(frame)
 
-        client.publish('Cam1', data, qos=0, retain=False)    # 填发布主题的名字
+        # client.publish('Cam1', data, qos=0, retain=False)    # 填发布主题的名字
         cv2.imshow("Test Frame", frame)
         cv2.waitKey(1)
-        time.sleep(6)
-        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) +
-              "   " + f"{height}h {width}w   " + f"size: {len(data)/1024:.4f} kB")
+        time.sleep(0)
+        # print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) +
+        #       "   " + f"{height}h {width}w   " + f"size: {len(data)/1024:.4f} kB")
 
 
-def OnConnect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+# def OnConnect(client, userdata, flags, rc):
+#     print("Connected with result code " + str(rc))
 
 
-def ClientConnect():
-    client.username_pw_set("CCTV_S", "8qADTw1m6ilcHg0pVSmh")    # 填MQTT账号密码
-    client.on_connect = OnConnect
-    client.connect(HOST, PORT, 60)
-    client.loop_start()
+# def ClientConnect():
+#     client.username_pw_set("CCTV_S", "8qADTw1m6ilcHg0pVSmh")    # 填MQTT账号密码
+#     client.on_connect = OnConnect
+#     client.connect(HOST, PORT, 60)
+#     client.loop_start()
 
 
 if __name__ == '__main__':
-    client = mqtt.Client(clientID)
-    ClientConnect()
+    # client = mqtt.Client(clientID)
+    # ClientConnect()
     SendingImage()
